83adb82a575d353ceb8d09b0ffd98ecaf7c647bc
Daniel Flöter <d.floe@gmx.de>
2019-01-13 21:39:33 +0100

    version 4.8.1

--- excerpt-extension.php
+++ excerpt-extension.php
@@ -4,7 +4,7 @@ Plugin Name: Excerpt Extension
 Plugin URI: http://tiptoppress.com/downloads/term-and-category-based-posts-widget/
 Description: Adds more excerpt options to the details pannel in the widgets admin from the premium widget Term and Category Based Posts Widget.
 Author: TipTopPress
-Version: 5.0
+Version: 4.8.1
 Author URI: http://tiptoppress.com
 */
 
